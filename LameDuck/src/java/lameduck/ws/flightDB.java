961eee5c04f2d963c58f2205941bfabf0bf4d512
Jóannes í Sandagerði <fragflop@yahoo.dk>
2013-11-05 14:45:05 +0100

    Made many changes to both LameDuck and Tests

---
+++ LameDuck/src/java/lameduck/ws/flightDB.java
@@ -0,0 +1,125 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lameduck.ws;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+/**
+ *
+ * @author moonlight
+ */
+ public class flightDB {
+
+        flightdata.FlightInfoListType flightInfoList = new flightdata.FlightInfoListType();
+
+        public flightDB() {
+
+            flightdata.FlightType flight1 = new flightdata.FlightType();
+            flight1.setCarrier("SAS");
+            flight1.setStartAirpot("Copenhagen, Denmark");
+            flight1.setDestinationAirport("London, Heathrow, England");
+            flight1.setDatetimeLift(date("2013-09-18T17:00:00.000+00:00"));
+            flight1.setDatetimeLanding(date("2013-09-18T19:00:00.000+00:00"));
+
+            flightdata.FlightType flight2 = new flightdata.FlightType();
+            flight2.setCarrier("SAS");
+            flight2.setStartAirpot("Copenhagen, Denmark");
+            flight2.setDestinationAirport("Beijing, China");
+            flight2.setDatetimeLift(date("2013-12-02T12:00:00.000+00:00"));
+            flight2.setDatetimeLanding(date("2013-12-03T19:30:00.000+00:00"));
+
+            flightdata.FlightType flight3 = new flightdata.FlightType();
+            flight3.setCarrier("British Airways");
+            flight3.setStartAirpot("Copenhagen, Denmark");
+            flight3.setDestinationAirport("Amsterdam, Netherlands");
+            flight3.setDatetimeLift(date("2013-12-05T12:00:00.000+00:00"));
+            flight3.setDatetimeLanding(date("2013-12-05T14:30:00.000+00:00"));
+
+            flightdata.FlightType flight4 = new flightdata.FlightType();
+            flight4.setCarrier("British Airways");
+            flight4.setStartAirpot("Copenhagen, Denmark");
+            flight4.setDestinationAirport("Oslo, Norway");
+            flight4.setDatetimeLift(date("2013-12-06T12:00:00.000+00:00"));
+            flight4.setDatetimeLanding(date("2013-12-06T14:30:00.000+00:00"));
+
+            flightdata.FlightType flight5 = new flightdata.FlightType();
+            flight5.setCarrier("British Airways");
+            flight5.setStartAirpot("Copenhagen, Denmark");
+            flight5.setDestinationAirport("Oslo, Norway");
+            flight5.setDatetimeLift(date("2013-12-06T12:00:00.000+00:00"));
+            flight5.setDatetimeLanding(date("2013-12-06T14:30:00.000+00:00"));
+
+            flightdata.FlightType flight6 = new flightdata.FlightType();
+            flight6.setCarrier("British Airways");
+            flight6.setStartAirpot("Copenhagen, Denmark");
+            flight6.setDestinationAirport("Oslo, Norway");
+            flight6.setDatetimeLift(date("2013-12-06T12:00:00.000+00:00"));
+            flight6.setDatetimeLanding(date("2013-12-06T14:30:00.000+00:00"));
+
+
+
+            flightdata.FlightInfoType flightInfo1 = new flightdata.FlightInfoType();
+            flightInfo1.setBookingNumber("1234567");
+            flightInfo1.setFlight(flight1);
+            flightInfo1.setFlightPrice(1500);
+            flightInfo1.setAirlineReservationServiceName("LameDuck");
+
+            flightdata.FlightInfoType flightInfo2 = new flightdata.FlightInfoType();
+            flightInfo2.setBookingNumber("1234568");
+            flightInfo2.setFlight(flight2);
+            flightInfo2.setFlightPrice(7500);
+            flightInfo2.setAirlineReservationServiceName("LameDuck");
+
+            flightdata.FlightInfoType flightInfo3 = new flightdata.FlightInfoType();
+            flightInfo3.setBookingNumber("1234570");
+            flightInfo3.setFlight(flight3);
+            flightInfo3.setFlightPrice(1030);
+            flightInfo3.setAirlineReservationServiceName("LameDuck");
+
+            flightdata.FlightInfoType flightInfo4 = new flightdata.FlightInfoType();
+            flightInfo4.setBookingNumber("1234571");
+            flightInfo4.setFlight(flight4);
+            flightInfo4.setFlightPrice(750);
+            flightInfo4.setAirlineReservationServiceName("LameDuck");
+
+            flightdata.FlightInfoType flightInfo5 = new flightdata.FlightInfoType();
+            flightInfo5.setBookingNumber("1234572");
+            flightInfo5.setFlight(flight5);
+            flightInfo5.setFlightPrice(750);
+            flightInfo5.setAirlineReservationServiceName("LameDuck");
+
+            flightdata.FlightInfoType flightInfo6 = new flightdata.FlightInfoType();
+            flightInfo6.setBookingNumber("1234573");
+            flightInfo6.setFlight(flight6);
+            flightInfo6.setFlightPrice(750);
+            flightInfo6.setAirlineReservationServiceName("LameDuck");
+
+            flightInfoList.getFlightInfo().add(flightInfo1);
+            flightInfoList.getFlightInfo().add(flightInfo2);
+            flightInfoList.getFlightInfo().add(flightInfo3);
+            flightInfoList.getFlightInfo().add(flightInfo4);
+            flightInfoList.getFlightInfo().add(flightInfo5);
+            flightInfoList.getFlightInfo().add(flightInfo6);
+
+        }
+
+        public XMLGregorianCalendar date(String dateinput) {
+
+            //Demos and Usage of javax.xml.datatype.DatatypeFactory.newXMLGregorianCalendar
+            //(int year,int month,int day,int hour,int minute,int second,int millisecond,int timezone)
+            DatatypeFactory df = null;
+            try {
+                df = DatatypeFactory.newInstance();
+            } catch (DatatypeConfigurationException ex) {
+                Logger.getLogger(lameDuckService.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            XMLGregorianCalendar date = df.newXMLGregorianCalendar(dateinput);
+            return date;
+        }
+    }
--- lameduckTest/test/lameduckJUnitTest.java
+++ lameduckTest/test/lameduckJUnitTest.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 
+import dk.dtu.imm.fastmoney.CreditCardFaultMessage;
 import flightdata.FlightInfoListType;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,39 +17,139 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import ws.lameduck.BookFlightFault;
+import ws.lameduck.CancelFlightFault;
 
 /**
  *
  * @author s114671
  */
 public class lameduckJUnitTest {
-    
+
     public lameduckJUnitTest() {
     }
-@Test
-    public void  testGetFlights() {
+
+    @Test
+    public void testGetFlightsCPH_LONDON() {
         ws.lameduck.LameDuckService service = new ws.lameduck.LameDuckService();
         ws.lameduck.LameDuckPortType port = service.getLameDuckPortTypeBindingPort();
-        flightdata.FlightInfoListType testResult=port.getFlights("", "", date("2013-12-06T14:30:00.000+00:00"));
-        
+        flightdata.FlightInfoListType testResult = port.getFlights("Copenhagen, Denmark", "London, Heathrow, England", date("2013-09-18T17:00:00.000+00:00"));
+
+        assertEquals("1234567", testResult.getFlightInfo().get(0).getBookingNumber());
+    }
+    
+    @Test
+    public void testGetFlightsCPH_BEIJING() {
+        ws.lameduck.LameDuckService service = new ws.lameduck.LameDuckService();
+        ws.lameduck.LameDuckPortType port = service.getLameDuckPortTypeBindingPort();
+        flightdata.FlightInfoListType testResult = port.getFlights("Copenhagen, Denmark", "Beijing, China", date("2013-12-02T12:00:00.000+00:00"));
+        System.out.println("SIZE: "+testResult.getFlightInfo().size());
+        assertEquals("1234568", testResult.getFlightInfo().get(0).getBookingNumber());
+    }
+    
+    @Test
+    public void testGetFlightsCPH_OSLO() {
+        ws.lameduck.LameDuckService service = new ws.lameduck.LameDuckService();
+        ws.lameduck.LameDuckPortType port = service.getLameDuckPortTypeBindingPort();
+        flightdata.FlightInfoListType testResult = port.getFlights("Copenhagen, Denmark", "Oslo, Norway", date("2013-12-06T12:00:00.000+00:00"));
+
+        assertEquals(testResult.getFlightInfo().get(0).getBookingNumber(), "1234571");
+    }
+    
+    @Test
+    public void testGetFlightsCPH_OSLO_MULTIPLERESULTS() {
+        ws.lameduck.LameDuckService service = new ws.lameduck.LameDuckService();
+        ws.lameduck.LameDuckPortType port = service.getLameDuckPortTypeBindingPort();
+        flightdata.FlightInfoListType testResult = port.getFlights("Copenhagen, Denmark", "Oslo, Norway", date("2013-12-06T12:00:00.000+00:00"));
         for(int i=0;i<testResult.getFlightInfo().size();i++){
-             System.out.println("all start flight airport:"+testResult.getFlightInfo().get(i).getFlight().getStartAirpot());
-        
+            System.out.println(testResult.getFlightInfo().get(i).getBookingNumber());       
         }
-    } 
+        assertEquals(testResult.getFlightInfo().size(), 3);
+    }
 
+    
+    //Helper method to convert a string like "2013-12-06T14:30:00.000+00:00" in to an XMLGregorianCalendar
     public XMLGregorianCalendar date(String dateinput) {
-            
-            //Demos and Usage of javax.xml.datatype.DatatypeFactory.newXMLGregorianCalendar
-            //(int year,int month,int day,int hour,int minute,int second,int millisecond,int timezone)
-            DatatypeFactory df = null;
-            try {
-                df = DatatypeFactory.newInstance();
-            } catch (DatatypeConfigurationException ex) {
-              //  Logger.getLogger(lameduckService.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            XMLGregorianCalendar date = df.newXMLGregorianCalendar(dateinput);
-            return date;
 
+        //Demos and Usage of javax.xml.datatype.DatatypeFactory.newXMLGregorianCalendar
+        //(int year,int month,int day,int hour,int minute,int second,int millisecond,int timezone)
+        DatatypeFactory df = null;
+        try {
+            df = DatatypeFactory.newInstance();
+        } catch (DatatypeConfigurationException ex) {
+            //  Logger.getLogger(lameduckService.class.getName()).log(Level.SEVERE, null, ex);
         }
+        XMLGregorianCalendar date = df.newXMLGregorianCalendar(dateinput);
+        return date;
+
+    }
+
+    @Test
+    public void testValidateCreditCard() throws CreditCardFaultMessage {
+        dk.dtu.imm.fastmoney.BankService service = new dk.dtu.imm.fastmoney.BankService();
+        dk.dtu.imm.fastmoney.BankPortType port = service.getBankPort();
+        dk.dtu.imm.fastmoney.types.CreditCardInfoType creditcardInfo = new dk.dtu.imm.fastmoney.types.CreditCardInfoType();
+        dk.dtu.imm.fastmoney.types.ExpirationDateType expDate = new dk.dtu.imm.fastmoney.types.ExpirationDateType();
+        expDate.setMonth(5);
+        expDate.setYear(9);
+
+        creditcardInfo.setName("Anne Strandberg");
+        creditcardInfo.setNumber("50408816");
+        creditcardInfo.setExpirationDate(expDate);
+        boolean result = port.validateCreditCard(02, creditcardInfo, 10000000);
+        System.out.println(result);
+        
+        dk.dtu.imm.fastmoney.types.CreditCardInfoType creditcardInfo2 = new dk.dtu.imm.fastmoney.types.CreditCardInfoType();
+        dk.dtu.imm.fastmoney.types.ExpirationDateType expDate2 = new dk.dtu.imm.fastmoney.types.ExpirationDateType();
+        expDate2.setMonth(7);
+        expDate2.setYear(9);
+
+        creditcardInfo2.setName("Bech Camilla");
+        creditcardInfo2.setNumber("50408822");
+        creditcardInfo2.setExpirationDate(expDate2);
+        
+        boolean result2 = port.validateCreditCard(02, creditcardInfo2,999);
+        System.out.println(result2);
+    }
+    
+    public void testBookFlight() throws BookFlightFault {
+        ws.lameduck.LameDuckService service = new ws.lameduck.LameDuckService();
+        ws.lameduck.LameDuckPortType port = service.getLameDuckPortTypeBindingPort();
+        //not enough money creditcard
+        dk.dtu.imm.fastmoney.types.CreditCardInfoType creditcardInfo2 = new dk.dtu.imm.fastmoney.types.CreditCardInfoType();
+        dk.dtu.imm.fastmoney.types.ExpirationDateType expDate2 = new dk.dtu.imm.fastmoney.types.ExpirationDateType();
+        expDate2.setMonth(7);
+        expDate2.setYear(9);
+        creditcardInfo2.setName("Bech Camilla");
+        creditcardInfo2.setNumber("50408822");
+        creditcardInfo2.setExpirationDate(expDate2);
+        //enough money creditcard
+        dk.dtu.imm.fastmoney.types.CreditCardInfoType creditcardInfo = new dk.dtu.imm.fastmoney.types.CreditCardInfoType();
+        dk.dtu.imm.fastmoney.types.ExpirationDateType expDate = new dk.dtu.imm.fastmoney.types.ExpirationDateType();
+        expDate.setMonth(5);
+        expDate.setYear(9);
+        creditcardInfo.setName("Anne Strandberg");
+        creditcardInfo.setNumber("50408816");
+        creditcardInfo.setExpirationDate(expDate);
+        ///book the flight
+        boolean result= port.bookFlight("1234567", creditcardInfo);
+        System.out.println("test book flight : "+result);
+    }
+    @Test
+    public void testCancelFlight() throws CancelFlightFault, BookFlightFault {
+        testBookFlight();
+        ws.lameduck.LameDuckService service = new ws.lameduck.LameDuckService();
+        ws.lameduck.LameDuckPortType port = service.getLameDuckPortTypeBindingPort();
+        
+        dk.dtu.imm.fastmoney.types.CreditCardInfoType creditcardInfo = new dk.dtu.imm.fastmoney.types.CreditCardInfoType();
+        dk.dtu.imm.fastmoney.types.ExpirationDateType expDate = new dk.dtu.imm.fastmoney.types.ExpirationDateType();
+        expDate.setMonth(5);
+        expDate.setYear(9);
+        creditcardInfo.setName("Anne Strandberg");
+        creditcardInfo.setNumber("50408816");
+        creditcardInfo.setExpirationDate(expDate);
+        
+        boolean result= port.cancelFlight("1234567", 1500, creditcardInfo);
+        System.out.println("test cancel flight result is : "+ result);
+    }
+    
 }
