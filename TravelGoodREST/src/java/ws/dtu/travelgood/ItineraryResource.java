903c610fdfb6ebbb708cff31ca5ec90883753490
helios2905 <helios_lie@hotmail.com>
2013-11-21 11:45:49 +0100

    Add itineraryResource

--- TravelGoodREST/src/java/ws/dtu/travelgood/ItineraryResource.java
+++ TravelGoodREST/src/java/ws/dtu/travelgood/ItineraryResource.java
@@ -1,12 +1,12 @@
 package ws.dtu.travelgood;
 
-import hotelreservationtypes.HotelList;
-import hotelreservationtypes.HotelQuery;
+import java.util.HashMap;
+import java.util.Map;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import travelgoodtypes.Itinerary;
 
@@ -14,21 +14,28 @@ import travelgoodtypes.Itinerary;
  *
  * @author prasopes
  */
+@Path("/")
 public class ItineraryResource {
 
-    @GET
+    Map<String,Itinerary> itineraryMap = new HashMap<String,Itinerary>();
+    int itineraryIndex = 1;
+    
+    @POST
     @Path("itineraries")
     @Produces (MediaType.APPLICATION_XML)
     public String createItinerary() {
-        return "12345";
+        Itinerary itinerary = new Itinerary();
+        itinerary.setItineraryNo(""+itineraryIndex);
+        itineraryIndex++;
+        itineraryMap.put(itinerary.getItineraryNo(),itinerary);
+        return itinerary.getItineraryNo();
     }
     
     @GET
     @Path("itinerary/{itineraryNo}")
     @Produces (MediaType.APPLICATION_XML)
     public Itinerary getItinerary(@PathParam("itineraryNo") String itineraryNo) {
-        System.out.println(itineraryNo);
-        return null;
+        return itineraryMap.get(itineraryNo);
     }
     
 }
