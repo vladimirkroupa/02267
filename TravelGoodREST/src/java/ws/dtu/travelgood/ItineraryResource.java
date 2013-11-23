5a6842732a760e5c66f02b23f90e2acd09fdf82c
helios2905 <helios_lie@hotmail.com>
2013-11-23 14:58:29 +0100

    Add method for addFlight in the FlightResource

--- TravelGoodREST/src/java/ws/dtu/travelgood/ItineraryResource.java
+++ TravelGoodREST/src/java/ws/dtu/travelgood/ItineraryResource.java
@@ -1,23 +1,26 @@
 package ws.dtu.travelgood;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import flightdata.FlightInfoType;
 import flightdata.FlightType;
-import hotelreservationtypes.HotelList;
-import hotelreservationtypes.HotelType;
 import hotelservice._02267.dtu.dk.wsdl.BookHotelOperationFault;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import travelgoodtypes.Itinerary;
 import travelgoodtypes.StatusType;
-import java.util.logging.Logger;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import travelgoodtypes.FlightBooking;
 
@@ -39,6 +42,7 @@ public class ItineraryResource {
         itinerary.setItineraryNo(nextItineraryNo());
         itinerary.setItineraryStatus(StatusType.UNCONFIRMED);
 
+        /*
         FlightBooking f = new FlightBooking();
         FlightInfoType fInfo = new FlightInfoType();
 
@@ -54,6 +58,7 @@ public class ItineraryResource {
 
         f.setFlightBooking(fInfo);
         f.setFlightBookingStatus(StatusType.CONFIRMED);
+        */
         //itinerary.getFlightBookingList().add();
 
         itineraryMap.put(itinerary.getItineraryNo(), itinerary);
@@ -80,6 +85,37 @@ public class ItineraryResource {
 //            HotelBookingOffers.addOfferedBooking(booking);
 //        }
 //    }
+    
+    @PUT
+    @Path("itinerary/{itineraryNo}")
+    @Produces (MediaType.APPLICATION_XML)    
+    public Response addFlight(@PathParam("itineraryNo") String itineraryNo, @QueryParam("flightBookingNo") String flightBookingNo){
+        Itinerary itinerary = itineraryMap.get(itineraryNo);
+        
+        //check in the flightBooking if they already exist
+        boolean bookingAlreadyExist = false;
+        ArrayList flightBookingList = (ArrayList) itinerary.getFlightBookingList();
+        for (Object o : flightBookingList){
+            FlightBooking booking = (FlightBooking) o;
+            FlightInfoType fInfo = booking.getFlightBooking();
+            if(fInfo.getBookingNumber().equals(flightBookingNo)){
+                bookingAlreadyExist = true;
+                break;
+            }
+        }
+        
+        if(!bookingAlreadyExist){            
+            FlightInfoType flight = FlightBookingData.findOfferedBooking(flightBookingNo);
+            
+            if(flight!=null){
+                FlightBooking booking = new FlightBooking();
+                booking.setFlightBooking(flight);
+                booking.setFlightBookingStatus(StatusType.UNCONFIRMED);
+                itinerary.getFlightBookingList().add(booking);
+            }
+        }
+        return Response.ok().entity(itinerary).build();
+    }     
     
     @GET
     @Path("itinerary/{itineraryNo}")
