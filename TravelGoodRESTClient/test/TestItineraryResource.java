e472fc70c8b26517b4abf4652e07236dc0c8a9c8
Vladimír Kroupa <vladimirkroupa@gmail.com>
2013-11-23 15:11:25 +0100

    addFlight test

--- TravelGoodRESTClient/test/TestItineraryResource.java
+++ TravelGoodRESTClient/test/TestItineraryResource.java
@@ -1,9 +1,12 @@
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import javax.ws.rs.core.MultivaluedMap;
 import org.junit.Test;
 import travelgoodtypes.Itinerary;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import travelgoodtypes.FlightBooking;
 import travelgoodtypes.StatusType;
 
 /**
@@ -17,7 +20,7 @@ public class TestItineraryResource {
     }
     
     @Test
-    public void testCreateItineary(){
+    public void testCreateItineary() {
         Client client = new Client();
         WebResource webResource = client.resource(ITINERARIES);
         String itineraryNo = webResource.post(String.class);
@@ -36,5 +39,32 @@ public class TestItineraryResource {
         
         assertEquals(StatusType.UNCONFIRMED, itinerary.getItineraryStatus());
         assertNotNull(itinerary.getItineraryNo());
+    }
+    
+    @Test
+    public void testAddFlightToItinerary() {
+        Client client = new Client();
+        WebResource webResource = client.resource(ITINERARIES);
+        String itineraryNo = webResource.post(String.class);
+        assertNotNull(itineraryNo);
+        
+        webResource = client.resource("http://localhost:8080/TravelGoodREST/webresources/flights");
+        MultivaluedMap param = new MultivaluedMapImpl();
+        param.add("date","2013-09-18");
+        param.add("startDest","Copenhagen, Denmark");
+        param.add("finalDest","London, Heathrow, England");
+        webResource.queryParams(param).get(String.class);
+                        
+        webResource = client.resource("http://localhost:8080/TravelGoodREST/webresources/itinerary/" + itineraryNo);
+        param = new MultivaluedMapImpl();
+        param.add("flightBookingNo", "1234567");   
+        Itinerary itinerary = webResource.queryParams(param).put(Itinerary.class);
+        
+        System.out.println(itinerary.getItineraryNo());
+        System.out.println(itinerary.getItineraryStatus());
+        
+        assertEquals(StatusType.UNCONFIRMED, itinerary.getItineraryStatus());
+        assertNotNull(itinerary.getItineraryNo());
+        assertTrue(itinerary.getFlightBookingList().size() > 0);        
     }
 }
