e4f353f8959f1c87aabcd285a796b40c3b906433
Vladimír Kroupa <vladimirkroupa@gmail.com>
2013-11-23 16:26:45 +0100

    Updated test.

--- TravelGoodRESTClient/test/TestItineraryResource.java
+++ TravelGoodRESTClient/test/TestItineraryResource.java
@@ -1,9 +1,11 @@
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 import hotelreservationtypes.HotelList;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 import org.junit.Test;
 import travelgoodtypes.Itinerary;
 import static org.junit.Assert.*;
@@ -57,8 +59,12 @@ public class TestItineraryResource {
         webResource = client.resource("http://localhost:8080/TravelGoodREST/webresources/itinerary/" + itineraryNo);
         param = new MultivaluedMapImpl();
         param.add("flightBookingNo", "1234567");
-        Itinerary itinerary = webResource.queryParams(param).put(Itinerary.class);
+        ClientResponse res = webResource.queryParams(param).put(ClientResponse.class);
+        assertEquals(Response.Status.OK.getStatusCode(), res.getStatus());
 
+        webResource = client.resource("http://localhost:8080/TravelGoodREST/webresources/itinerary/" + itineraryNo);
+        Itinerary itinerary = webResource.get(Itinerary.class);
+        
         assertEquals(StatusType.UNCONFIRMED, itinerary.getItineraryStatus());
         assertEquals(itineraryNo, itinerary.getItineraryNo());
         assertTrue(itinerary.getFlightBookingList().size() > 0);
@@ -85,10 +91,40 @@ public class TestItineraryResource {
         webResource.queryParams(param).put(String.class);
 
         webResource = client.resource("http://localhost:8080/TravelGoodREST/webresources/itinerary/" + itineraryNo);
-        Itinerary itinerary = webResource.get(Itinerary.class);
-
+        ClientResponse res = webResource.get(ClientResponse.class);
+        Itinerary itinerary = res.getEntity(Itinerary.class);
+        
+        assertEquals(Response.Status.OK.getStatusCode(), res.getStatus());
+        
         assertEquals(StatusType.UNCONFIRMED, itinerary.getItineraryStatus());
         assertEquals(itineraryNo, itinerary.getItineraryNo());
         assertTrue(itinerary.getHotelBookingList().size() > 0);
     }
+    
+ 
+    @Test
+    public void testCancelItinerary() {
+        Client client = new Client();
+        WebResource webResource = client.resource(ITINERARIES);
+        String itineraryNo = webResource.post(String.class);
+        assertNotNull(itineraryNo);
+
+        webResource = client.resource(ITINERARIES);
+        String itineraryNo2 = webResource.post(String.class);
+        assertNotNull(itineraryNo2);
+        assertThat(itineraryNo, not(equalTo(itineraryNo2)));
+
+        webResource = client.resource("http://localhost:8080/TravelGoodREST/webresources/itinerary/" + itineraryNo);
+        Itinerary itinerary = webResource.get(Itinerary.class);
+
+        assertEquals(StatusType.UNCONFIRMED, itinerary.getItineraryStatus());
+        assertNotNull(itinerary.getItineraryNo());
+        
+        // cancel
+        
+        webResource = client.resource("http://localhost:8080/TravelGoodREST/webresources/itinerary/" + itineraryNo);
+        ClientResponse res = webResource.delete(ClientResponse.class);
+        assertEquals(Response.Status.OK.getStatusCode(), res.getStatus());
+    }
+    
 }
